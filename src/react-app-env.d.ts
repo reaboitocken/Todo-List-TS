ca658b80b18060555b53d4abbaa9171749b6798e
Petru <Patron209@mail.ru>
2022-08-30 18:22:55 +0300

    change types to react-app-env

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="react-scripts" />
+type Todo = {
+  text: string;
+  complete: boolean;
+};
+
+type ToggleComplete = (selectedTodo: Todo) => void;
+type AddTodo = (newTodo: string) => void;
+type RemoveTodo = (todoToRemove: Todo) => void;
+
+type Option = {
+  value: string;
+  onClick: () => void;
+  color?: string;
+};
